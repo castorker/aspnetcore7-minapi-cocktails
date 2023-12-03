3e3c8a2f687fbb0972e456cf3a583d41e103156b
Antonio Silva <ae.osilva@hotmail.com>
2023-12-03 13:40:45 +0100

    Added Get cocktails endpoint to retrieve all cocktails.

--- src/Cocktails/Cocktails.API/Program.cs
+++ src/Cocktails/Cocktails.API/Program.cs
@@ -16,22 +16,9 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/cocktails", async (CocktailsDbContext cocktailsDbContext) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return await cocktailsDbContext.Cocktails.ToListAsync();
 });
 
 // recreate & migrate the database on each run, for development purposes
@@ -43,8 +30,3 @@ using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().Create
 }
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
